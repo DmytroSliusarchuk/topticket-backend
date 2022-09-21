3197c51268cc4c9114254c2b8617a4155f1c099e
dima475 <dmitroslusarcuk@gmail.com>
2022-09-22 00:26:38 +0300

    Added Flask

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello world")
-print("test")
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    print("http://127.0.0.1:8080")
+    serve(app, host='127.0.0.1', port=8080)
