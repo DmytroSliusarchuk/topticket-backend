c8c79555182873bcf8630b9b43a6e7174ac8119a
dima475 <dmitroslusarcuk@gmail.com>
2022-11-02 15:41:38 +0200

    [Dima Sliusarchuk] Added user creation and validation

--- backend/resource/__init__.py
+++ backend/resource/__init__.py
@@ -1,3 +1,4 @@
 from backend.resource.resource_test import index
 from backend.resource.resource_test import hello_world
-from backend.resource.resource_test import test_add_user
+from backend.resource.auth import register
+from backend.resource.users import get_user_by_username
--- backend/resource/resource_test.py
+++ backend/resource/resource_test.py
@@ -1,5 +1,7 @@
 from backend.app import app
-from backend.models.user import User
+from backend.models.user import User, UserSchema
+from flask import jsonify
+from marshmallow import ValidationError
 
 
 @app.route("/")
@@ -10,14 +12,3 @@ def index():
 @app.route("/api/v1/hello-world-<int:num>")
 def hello_world(num):
     return f"<h1>Hello World {num}</h1>"
-
-
-@app.route("/test_add_user", methods=["GET"])
-def test_add_user():
-    user = User(idperson=1, username="dima", name="Dmytro"
-               ,surname="Sliusarchuk", city="Lviv", email="dmitroslusarcuk@gmail.com"
-              , password="qwerty1234", phone="+380673530318", role="Admin")
-
-    user.save_to_db()
-
-    return {"messege": "user was successfully created"}
